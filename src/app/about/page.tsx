18fde8d0033aeb84187478d78f5b955bb39ddf2a
samrawaseem <160887316+samrawaseem@users.noreply.github.com>
2025-02-06 21:28:53 +0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,15 +14,18 @@ export default function AboutPage() {
 
       <h2>📚 Education</h2>
       <ul className="education">
-        <li>Doctor of Pharmacy - Ziauddin University (2019)</li>
-        <li>B.Ed - Karachi University (2024)</li>
+        <li>B.ed in 2024 from Ziauddin University</li>
+        <li>Doctor Of Pharmacy in 2019 from Karachi University</li>
+        <li>Front-End Development from Tech Force Pakistan)</li>
+        <li>Digital Marketing from Tech Force Pakistan</li>
+        <li>Generative AI,Web3 and Metaverse(inprogress)from GIAIC</li>
       </ul>
 
       <h2>💼 Experience</h2>
       <ul className="experience">
-        <li>Pharmacist - Nigheban Compounding Pharmacy</li>
+        <li>superior Grammar School,khi</li>
         <li>Hospital Pharmacist - Ibn-e-Seena Hospital Complex</li>
-        <li>Lecturer - Superior Grammar School</li>
+        <li>R&D Department Pharmacist,Nigheban Compunding Pharmacy</li>
       </ul>
     </section>
   );
