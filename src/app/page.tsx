b1d61cf06fa92562fd8bd4652301e1b82dbc5dcd
samrawaseem <160887316+samrawaseem@users.noreply.github.com>
2025-02-06 20:44:18 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ContactPage from "./contact/page";
 export default function HomePage() {
   return (
     <section className="container">
-      <h1>👋 Hi, I'm Samra Waseem</h1>
+      <h1>{"👋 Hi, I'm Samra Waseem"}</h1>
       <p className="subtitle">
         A <span className="highlight">Pharmacist</span>, <span className="highlight">Front-End Developer</span>, and <span className="highlight">Lecturer</span>  
         blending healthcare, technology, and education.
